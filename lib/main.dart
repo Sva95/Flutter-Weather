5171c3f9d4fc51c83bdc8cd1845e70d921a5cc5e
Vadim Simonov <simonov.vadim95@gmail.com>
2021-03-28 14:32:00 +0200

    update readme.md

--- lib/main.dart
+++ lib/main.dart
@@ -7,21 +7,4 @@ void main() {
 }
 
 
-/*class MyApp extends StatelessWidget {
-  final PlayerRepository playerRepository;
-  MyApp({this.playerRepository});
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Bloc Implementation',
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        fontFamily: "GoogleSans",
-        primarySwatch: Colors.blue,
-      ),
-      home: HomePage(playerRepository:playerRepository),
-    );
-  }
-}*/
 
